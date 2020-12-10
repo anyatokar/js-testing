b2d69d9c3492333c053dee44dbfa9c6205820d2e
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2020-12-10 12:21:04 -0500

    pangram completed

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,5 +1,21 @@
 const isPangram = function(text) {
+  text = text.toLowerCase()
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  const hashMap = {}
 
+  for (let i = 0; i < alphabet.length; i++) {
+    hashMap[alphabet[i]] = 0;
+  }
+
+  for (let i = 0; i < text.length; i++) {
+    hashMap[text[i]] += 1;
+  }
+
+  for (const key in hashMap) {
+    if (hashMap[key] === 0) {
+      return false }
+  }
+  return true
 };
 
 module.exports = isPangram;
--- test/is_pangram.test.js
+++ test/is_pangram.test.js
@@ -9,55 +9,61 @@ describe('isPangram()', () => {
     // Arrange
     const text = 'the quick brown fox jumps over the lazy dog';
 
-    // Act
-
-    // Assert
+    // Act // Assert
+    expect(isPangram(text)).toBeTruthy()
   });
 
   test('works with "abcdefghijklmnopqrstuvwxyz"', () => {
     // Arrange
-
-    // Act
-
-    // Assert
+    const text = 'abcdefghijklmnopqrstuvwxyz';
+    // Act // Assert
+    expect(isPangram(text)).toBeTruthy()
 
   });
 
   test("missing character 'x'", () => {
     // Arrange
+    const missingX = 'abcdefghijklmnopqrstuvwyz';
 
-    // Act
-
-    // Assert
+    // Act // Assert
+    expect(isPangram(missingX)).toBeFalsy();
 
   });
 
   test('empty sentence', () => {
     // Arrange
-
-    // Act
-
-    // Assert
+    const text = ''
+    // Act // Assert
+    expect(isPangram(text)).toBeFalsy();
 
   });
 
   test('pangram with underscores instead of spaces works', () => {
     // Arrange
+    const text = 'the_quick_brown_fox_jumps_over_the_lazy_dog';
 
-    // Act
-
-    // Assert
-
+    // Act // Assert
+    expect(isPangram(text)).toBeTruthy()
   });
 
   test('pangram with numbers', () => {
     // Arrange
+    const nums = '123';
+    const text = 'abcdefghijklmnopqrstuvwxyz';
 
     // Act
-
     // Assert
+    expect(isPangram(nums)).toBeFalsy();
+    expect(isPangram(text)).toBeTruthy()
 
   });
 
   // Write your own test case
+  test('works with capital letters', () => {
+    const textCaps = 'AbcdefghiJklmnopqrstuvwxyz';
+    const textCapsMissingX = 'AbcdefghiJklmnopqrstuvwyz';
+
+    expect(isPangram(textCaps)).toBeTruthy();
+    expect(isPangram(textCapsMissingX)).toBeFalsy();
+  })
 });
